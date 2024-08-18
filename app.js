f82ac65a662fb5a2583fa2d08a526d6a0b9f9d6d
hassaanhassaan <hassaan2980@gmail.com>
2024-08-19 02:18:45 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,259 @@
+
+
+// // Q1
+// alert("Hello Everyone!");
+
+
+
+// // Q2
+// alert("please enter a valid password")
+
+
+
+// // Q3
+// alert("Welcome to my website \n Hassaan's Website");
+
+
+
+// // Q4
+// alert("I am Hassaan");
+// alert("I am 20 year old");
+// alert("I learning WD");
+
+
+
+// // Q5
+
+
+
+// // Q6
+
+
+
+// // Q7
+// var username;
+
+
+
+// //Q8
+// var myname = "Hassaan";
+// console.log(myname);
+
+
+
+// // Q9
+
+// // (a)
+// var message;
+
+// // (b)
+// message = "Hello";
+
+// // (c)
+// alert(message);
+
+
+
+// // Q10
+// var name = "Hassaan"
+// var clas = "10th class"
+// var group = "science"
+// var bioData = name + "\n" + clas + "\n" + group;
+// alert(bioData);
+
+
+
+
+// // Q11
+// var email = "hassaan2980@gmail.com"
+// alert("My email adress is \n" + email)
+
+
+
+// // // Q12
+// console.log(email);
+
+
+
+// // // Q13
+// var emoji = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(emoji)
+// console.log(emoji)
+
+
+
+// // Q14
+// var age = "Age: 20"
+// alert(age)
+
+
+
+// // Q15
+// var count = +prompt("How many times have you visited this website")
+// console.log("user visited this website " + count + " times.")
+// alert("You have visited this website " + count + " times.")
+
+
+
+// // Q16
+// var birthyear = "28th July, 2004"
+// console.log(birthyear)
+
+
+
+// // Q17
+// alert("Hassaan's cloths Shop")
+// var vis_name = prompt("What is your name?")
+// var product = prompt( vis_name + " ,what type of clothing do you want?")
+// var color = prompt( vis_name + " ,what color do you want of the " + product + "?")
+// var quantity = +prompt( vis_name + " ,how many " + color + "-" + product + " do you want?")
+// var confirmation = prompt( vis_name + " ,your final order is " + quantity + "-" + color + "-" + product + " \n Is that ok?")
+// if (confirmation === "yes"){
+//     alert("Your order is confirmed.")
+//     console.log( vis_name + "'s order confirmed of " + quantity + "-" + color + "-" + product + ".")
+// }else if (confirmation === "ok"){
+//     alert("Your order is confirmed.")
+//     console.log( vis_name + "'s order confirmed of " + quantity + "-" + color + "-" + product + ".")
+// }
+// else {
+//     alert("Your order is not confirmed")
+//     console.log( vis_name + "'s order not confirmed of " + quantity + "-" + color + "-" + product + ".")
+// }
+
+
+
+// // Q18
+// var country = "pakistan", province = "sindh", city = "karachi"
+// alert(country,province,city)
+// console.log(country,province,city)
+
+
+
+// //Q19
+// //Legal Variables
+// var abc;
+// var $abc;
+// var a_bc;
+// var aBc123;
+
+// // ILLEGAL Variables
+// var 'abc';
+// var 123abc;
+// var ab c;
+// var class;
+// var @abc;
+
+
+
+// //Q20
+// //a
+// document.write("<h1>Rules for naming JS variable </h1>");
+// //b
+// document.write("Variable names can only contain Letters,Underscore and Dollar signs. For example $my_1stVariable ");
+// //c
+// document.write("Variables must begin with a Dollar signs, underscore and Letters. For example $name, _name or name");
+// //d
+// document.write("Variable names are case Sensitive");
+//  //e
+// document.write("Variable names should not be JS Reserved Words Examples of such keywords include if, else, for, while, function and var.");
+
+
+
+// //Q21
+// var number1 = +prompt("Enter a First Number for add");
+// var number2 = +prompt("Enter a Second Number for add");
+// var result = console.log(number1 + number2);
+
+
+
+// // Q22
+
+// //Subtraction
+// var num1 = +prompt("Enter a First Number for subtraction");
+// var num2 = +prompt("Enter a Second Number for subtraction");
+// var result1 = console.log(num1 - num2);
+
+// //Multiplication
+// var num1 = +prompt("Enter a First Number for multiplication");
+// var num2 = +prompt("Enter a Second Number for multiplication");
+// var result2 = console.log(num1 * num2);
+
+// //Division
+// var num1 = +prompt("Enter a First Number for divide");
+// var num2 = +prompt("Enter a Second Number for divide");
+// var result3 = console.log(num1 / num2);
+
+// //Modulus
+// var num1 = +prompt("Enter a First Number for modilus");
+// var num2 = +prompt("Enter a Second Number for modulus");
+// var result3 = console.log(num1 % num2);
+
+
+
+// //Q23
+
+// //a 
+// var a
+
+// //b
+// var b = 5 
+// document.write("Value after variable declaration is: " + b + "<br>");
+
+// //c
+// var c = 10;
+
+// //d
+// document.write("Initial value: " + c + "<br>");
+
+//e
+a++;
+
+//f
+console.log("Value after increment is:",a )
+
+//g
+var g =+ 7;
+
+//h
+ document.write("Value after addition is: " + g + "<br>");
+
+//i
+a--
+
+//j
+console.log("Value after decrement is:",a )
+
+// //k
+// var c = b % 3;
+// var remainder = c % 3;
+
+// //l
+// console.log("The remainder is: ",remainder)
+
+
+
+// // Q24
+// var ticket = 600;
+// var cost_five_tickets = 600 * 5;
+// console.log(cost_five_tickets);
+
+
+
+// //Q25
+
+// alert("Table of 10")
+
+// document.write("<b>Table of 10</b><br/>")
+
+// var a = 10
+
+// document.write("<br/>10 x 1 =" + a*1)
+// document.write("<br/>10 x 2 =" + a*2)
+// document.write("<br/>10 x 3 =" + a*3)
+// document.write("<br/>10 x 4 =" + a*4)
+// document.write("<br/>10 x 5 =" + a*5)
+// document.write("<br/>10 x 6 =" + a*6)
+// document.write("<br/>10 x 7 =" + a*7)
+// document.write("<br/>10 x 8 =" + a*8)
+// document.write("<br/>10 x 9 =" + a*9)
+// document.write("<br/>10 x 10 =" + a*10)
